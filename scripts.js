2ad10d95049fd1eb454b3340820e86efe7951716
qipi <ebroberts9@icloud.com>
2024-10-17 23:04:56 +0100

    from pc
    yuhhh

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function bin() {
+  document.getElementById("binImg").style.position="absolute"; 
+  let vh = window.innerHeight;
+  let vw = window.innerWidth;
+  var a = Math.floor((Math.random() * (vh-(vw * 0.2))) + 300);
+  var b = Math.floor((Math.random() * (vw-(vw * 0.2))) + 200);
+  var x = document.getElementById("binImg");
+    document.getElementById("binImg").style.top=a + "px"; 
+    document.getElementById("binImg").style.left=b + "px"; 
+  console.log(a)
+  console.log(b)
+}
+
+function spawn() {
+  var x = document.getElementById("start");
+    document.getElementById("spawn").style.onclick = bin();
+    document.getElementById("start").style.onclick = stop(); 
+} 
